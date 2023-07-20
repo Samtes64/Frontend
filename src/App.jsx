de722f558f523f2d42794afb975cdb7b3f2e1e40
Samuel Tesfaye <samtes64@gmail.com>
2023-07-20 14:25:16 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+
+
+function App() {
+
+
+  return (
+    <div className="bg-black">
+      <p className="bg-black">abebeb</p>
+    </div>
+  )
+}
+
+export default App
