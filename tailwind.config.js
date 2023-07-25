66da28d2741add088b189048e8a9621390e1d5c9
Samuel Tesfaye <samtes64@gmail.com>
2023-07-25 04:29:45 +0300

    tailwind and some dependencies

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,18 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins"],
+      },
+      colors: {
+        primary: "#1967D2",
+        secondary: "#B9CAE0",
+      },
+      screens: {
+        lmd: "850px",
+      },
+    },
   },
   plugins: [],
 }
